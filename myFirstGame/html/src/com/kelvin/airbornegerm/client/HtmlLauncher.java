f3ab92a4f296e5650424fc4f20ba3b1dffcb18a0
2khc <kh.kelvinchan@gmail.com>
2015-01-07 20:57:00 +0000

    created my first game
    rearranged directories as well

---
+++ myFirstGame/html/src/com/kelvin/airbornegerm/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.kelvin.airbornegerm.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.kelvin.airbornegerm.AirborneGerm;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new AirborneGerm();
+        }
+}
---
+++ myFirstGame/ios/src/com/kelvin/airbornegerm/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.kelvin.airbornegerm;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.kelvin.airbornegerm.AirborneGerm;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new AirborneGerm(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
